83f346a1695d068d38e429e9f96b09ca676a4375
Matheus Nascimento <contamatheusnascimento@gmail.com>
2018-04-24 15:04:55 -0300

    Adds the images subscriber structure

--- data_acquisition_node/include/zed_data_acquisition.hpp
+++ data_acquisition_node/include/zed_data_acquisition.hpp
@@ -19,17 +19,32 @@ class ZedAcquisition
     
     explicit ZedAcquisition();
     virtual ~ZedAcquisition();
-    void DepthImageCallback(const sensor_msgs::ImageConstPtr& msg);
+    bool readPrf(int &cols, int &rows, char *&data, const char *path);
+    bool readPrf(cv::Mat &frame, const char *path);
+    bool writePrf(int cols, int rows, unsigned char *&data, const char *path);
+    void depthImageCallback(const sensor_msgs::ImageConstPtr& msg);
+    void leftRawImageCallback(const sensor_msgs::ImageConstPtr& msg);
+    void rightRawImageCallback(const sensor_msgs::ImageConstPtr& msg);
 
   protected:
 
-    ros::NodeHandle nh_;
+    ros::NodeHandle nh_;                                  ///< ROS image transport object
     image_transport::ImageTransport img_transport_;       ///< ROS image transport object
     image_transport::Subscriber depth_sub_;               ///< ROS depth image subscriber
-    cv::VideoWriter *output_video_;
-    bool is_saving_; 
-    int frame_height_;
-    int frame_width_;
+    image_transport::Subscriber left_sub_;                ///< ROS left raw image subscriber
+    image_transport::Subscriber right_sub_;               ///< ROS right raw image subscriber
+
+    cv::VideoWriter *disparity_output_video_;                       ///< ROS 
+    cv::VideoWriter *right_output_video_;                       ///< ROS 
+    cv::VideoWriter *left_output_video_;                       ///< ROS 
+    cv::Mat right_image_;                                 ///< cv 
+    cv::Mat left_image_;                                  ///< cv 
+    cv::Mat raw_disparity_;                               ///< cv 
+
+    bool is_saving_;                                      ///< ROS 
+    int frame_height_;                                    ///< ROS 
+    int frame_width_;                                     ///< ROS 
+    int cont_;
 };
 
 } // zed namespace
--- data_acquisition_node/src/zed_data_acquisition.cpp
+++ data_acquisition_node/src/zed_data_acquisition.cpp
@@ -2,37 +2,128 @@
 
 namespace zed
 {
-    ZedAcquisition::ZedAcquisition()
-        : nh_("~")
-        , img_transport_(nh_)
-        , depth_sub_(img_transport_.subscribe("/zed/depth/depth_registered", 5,&ZedAcquisition::DepthImageCallback, this))
-        , output_video_(NULL)
-        , frame_height_(0)
-        , frame_width_(0)
-        , is_saving_(true)
-    {
-        
-    }
-    ZedAcquisition::~ZedAcquisition()
-    {
-    }
+ZedAcquisition::ZedAcquisition()
+    : nh_("~")
+    , img_transport_(nh_)
+    , depth_sub_(img_transport_.subscribe("/zed/depth/depth_registered", 1,&ZedAcquisition::depthImageCallback, this))
+    , left_sub_(img_transport_.subscribe("/zed/left/image_raw_color", 1,&ZedAcquisition::leftRawImageCallback, this))
+    , right_sub_(img_transport_.subscribe("/zed/right/image_raw_color", 1,&ZedAcquisition::rightRawImageCallback, this))
+    , disparity_output_video_(NULL)
+    , right_output_video_(NULL)
+    , left_output_video_(NULL)
+    , right_image_()
+    , left_image_()
+    , raw_disparity_()
+    , frame_height_(0)
+    , frame_width_(0)
+    , cont_(0)
+    , is_saving_(false)
+{
+    
+}
+ZedAcquisition::~ZedAcquisition()
+{
+}
 
-    void ZedAcquisition::DepthImageCallback(const sensor_msgs::ImageConstPtr& msg)
-    {
-        cv::Mat raw_disparity;
-        raw_disparity = cv_bridge::toCvShare(msg, "32FC1")->image;
+bool ZedAcquisition::readPrf(int &cols, int &rows, char *&data, const char *path)
+{
+	try
+	{
+		std::ifstream infile(path, std::ifstream::binary);
 
-        if (is_saving_)
-        {
-            int codec = CV_FOURCC('M', 'J', 'P', 'G');
-            output_video_ = new cv::VideoWriter("/home/matheus/video.avi",-1, 3, raw_disparity.size(), true);
-            frame_height_ = raw_disparity.rows;
-            frame_width_  = raw_disparity.cols;
-            is_saving_ = false;
-        }
-        std::cout << frame_width_ << " x " << frame_height_ << std::endl; 
-        output_video_->write(raw_disparity);
-        cv::imshow("view", raw_disparity);
-        cv::waitKey(30);
+		// read data as a block:
+		infile.read((char *)&cols, 4);
+		infile.read((char *)&rows, 4);
+
+		data = new char[rows * cols];
+		infile.read(data, rows * cols);
+	}
+	catch (std::exception &e)
+	{
+		return false;
+	}
+	return true;
+}
+
+bool ZedAcquisition::readPrf(cv::Mat &frame, const char *path)
+{
+
+	int cols, rows;
+	char *data;
+	bool success = readPrf(cols, rows, data, path);
+	frame = cv::Mat(cv::Size(cols, rows), CV_8UC1, data);
+
+	return success;
+}
+
+bool ZedAcquisition::writePrf(int cols, int rows, unsigned char *&data, const char *path)
+{
+	try
+	{
+		std::ofstream outfile(path, std::ios::out | std::ios::binary);
+		outfile.write((char *)&cols, 4);
+		outfile.write((char *)&rows, 4);
+		outfile.write((char *)data, cols * rows);
+		outfile.close();
+	}
+	catch (std::exception &e)
+	{
+		return false;
+	}
+	return true;
+}
+
+void ZedAcquisition::depthImageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    raw_disparity_ = cv_bridge::toCvShare(msg, "32FC1")->image;
+    cont_++;
+    std::ostringstream image_name;
+
+    if (is_saving_)
+    {
+        int codec = CV_FOURCC('M', 'J', 'P', 'G');
+        disparity_output_video_ = new cv::VideoWriter("/home/matheus/video.avi",-1, 3, raw_disparity_.size(), true);
+        is_saving_ = false;
     }
+    frame_height_ = raw_disparity_.rows;
+    frame_width_  = raw_disparity_.cols;
+    std::cout << frame_width_ << " x " << frame_height_ << std::endl; 
+    // disparity_output_video_->write(raw_disparity_);
+    cv::resize(raw_disparity_, raw_disparity_, cv::Size(640,360));
+    cv::imshow("view", raw_disparity_);
+    image_name << "/home/matheus/32bits/" << cont_ << ".png"; 
+    cv::imwrite(image_name.str(), raw_disparity_);
+    cv::waitKey(1);
+}
+
+void ZedAcquisition::leftRawImageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    cv::Mat left_image_;
+    left_image_ = cv_bridge::toCvShare(msg, "bgr8")->image;
+    if (is_saving_)
+    {
+        int codec = CV_FOURCC('M', 'J', 'P', 'G');
+        left_output_video_ = new cv::VideoWriter("/home/matheus/left.avi",codec, 3, left_image_.size(), true);
+        is_saving_ = false;
+    }
+    // left_output_video_->write(left_image_);
+    // cv::imshow("view", left_image_);
+    // cv::waitKey(1);
+}
+
+void ZedAcquisition::rightRawImageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+    
+    right_image_ = cv_bridge::toCvShare(msg, "bgr8")->image;
+    if (is_saving_)
+    {
+        int codec = CV_FOURCC('M', 'J', 'P', 'G');
+        right_output_video_ = new cv::VideoWriter("/home/matheus/right.avi",codec, 3, right_image_.size(), true);
+        is_saving_ = false;
+    }
+    // right_output_video_->write(right_image_);
+    // cv::imshow("view", right_image_);
+    // cv::waitKey(1);
+}
+
 }
